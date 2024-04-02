d5bcaefa50bdea9ce997ce109e14470db1b8b800
0rphee <0rph3e@proton.me>
2024-04-01 18:45:27 -0600

    ex 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (strToRepeat, nTimes) {
+    if (nTimes < 0) return "ERROR";
+    let newStr = "";
+    for (let i = 0; i < nTimes; i++){
+        newStr += strToRepeat;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
