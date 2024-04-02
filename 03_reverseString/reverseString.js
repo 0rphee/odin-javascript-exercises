83fc26b6ffbe7a381ce945c9e7aebbd9c6d52e25
0rphee <0rph3e@proton.me>
2024-04-01 18:45:31 -0600

    ex 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(strToReverse) {
+    let reversedStr = "";
+    for (let i = 1; i <= strToReverse.length; i++){
+        reversedStr += strToReverse[strToReverse.length - i];
+    }
+    return reversedStr;
 };
+
+const ttry = reverseString("1234");
+console.log(ttry);
 
 // Do not edit below this line
 module.exports = reverseString;
