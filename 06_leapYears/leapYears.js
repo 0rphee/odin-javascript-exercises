4ce17516ef30d8ea783eb18b3588b79d5b0b79be
0rphee <0rph3e@proton.me>
2024-04-02 12:41:12 -0600

    ex 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if they are divisible by 4, LEAP 
+    if (year % 4 === 0){
+        // if they are divisible by 100 they are NOT
+        if (year % 100 === 0 ){
+            // but if they are by 100 & 400 LEAP
+            if (year % 400 === 0) return true;
+            else return false;
+        } else return true;
+    } else return false;
 };
 
 // Do not edit below this line
