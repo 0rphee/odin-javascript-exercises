1f5bb6fd592287bb9fa7bcb7c426b12e84157edf
0rphee <0rph3e@proton.me>
2024-04-02 15:06:01 -0600

    ex 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+function calcAge(birthDate, deathDate){
+    return deathDate.getFullYear() - birthDate.getFullYear();
+}
 
+function calcAgeObj(obj){
+    let death = new Date();
+    if (obj.yearOfDeath){
+        death = new Date(obj.yearOfDeath, 0); 
+    } 
+    return calcAge(new Date(obj.yearOfBirth, 0), death);
+}
+
+const findTheOldest = function(arr) {
+    let bestObj;
+    for (nextObj of arr){
+         if (!bestObj){
+             bestObj = nextObj;
+         } else{
+             const x = calcAgeObj(bestObj); 
+             const y = calcAgeObj(nextObj); 
+             bestObj = x > y ? bestObj : nextObj;
+         }
+    }
+    return bestObj;
 };
 
 // Do not edit below this line
