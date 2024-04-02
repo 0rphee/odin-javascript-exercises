264ce7cf8772bfe0fe5fd9d338dbc4f2942d00dc
0rphee <0rph3e@proton.me>
2024-04-01 19:10:56 -0600

    ex 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    // error if any is negative
+    if ( x < 0 || y < 0) return "ERROR";
 
+    // error if any parameter is non-number
+    if ( typeof x !== "number" || typeof y !== "number" ) return "ERROR";
+
+    // in case the arguments are reversed, with this, the previous code still works
+    let initialNum = x;
+    let finalNum = y;
+    if (initialNum > finalNum){
+        const tmp = initialNum;
+        initialNum = finalNum;
+        finalNum = tmp;
+    }
+
+    // actual computing of the sum
+    let sum = 0;
+    for (let i = initialNum; i <= finalNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
