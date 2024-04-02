be5ebcc3dd99c070df4b21773dc7fec2b2974c40
0rphee <0rph3e@proton.me>
2024-04-02 13:41:02 -0600

    ex 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+    if (fibIndex < 0) return "OOPS";
+    if (fibIndex == 0) return 0;
 
+    let x = 1;
+    let y = 1;
+    for (let i = 0; i < fibIndex - 2; i++){
+        const temp = x + y;
+        x = y;
+        y = temp;
+    }
+    return y;
 };
 
 // Do not edit below this line
